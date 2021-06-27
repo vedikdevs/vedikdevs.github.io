f570a4ba7bb8f827b10b244c7cc2215f6c660de2
Himanshu Jangid <iamahalofan@gmail.com>
2021-06-27 17:32:55 +0530

    new updates

--- main.js
+++ main.js
@@ -34,44 +34,49 @@ mode.onclick = () => {
   mode.childNodes[1].name = currentMode;
   // console.log("modechanged")
 }
-// import * as Three from 'three'
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-// import { Scene } from 'three';
-// const scene = new Three.Scene()
-// const camera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-// const renderer = new Three.WebGLRenderer({
-//   canvas: document.getElementById("canvas")
-// })
+import * as Three from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+const scene = new Three.Scene()
+const camera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const renderer = new Three.WebGLRenderer({
+  canvas: document.getElementById("canvas")
+})
 
 // var controls = new OrbitControls(camera, renderer.domElement)
 
-// renderer.setPixelRatio(window.devicePixelRatio)
-// renderer.setSize(window.innerWidth, window.innerHeight)
-// camera.position.setZ(30)
-// scene.background = new Three.Color(0xffffff)
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setSize(window.innerWidth, window.innerHeight)
+camera.position.setZ(15)
+// camera.position.setY(10)
+scene.background = new Three.Color(0x121212)
 
-// /** @type {Three.Mesh} */
-// const loader = new GLTFLoader()
-// function modelLoader(url) {
-//   return new Promise((resolve, reject) => {
-//     loader.load(url, data=> resolve(data.scene), null, reject);
-//   });
-// }
-// var vedikDevsObject = await modelLoader('sq.glb');
-// scene.add(vedikDevsObject)
+/** @type {Three.Mesh} */
+const loader = new GLTFLoader()
+function modelLoader(url) {
+  return new Promise((resolve, reject) => {
+    loader.load(url, data=> resolve(data.scene), null, reject);
+  });
+}
+/** @type {Three.BoxGeometry} */
+var vedikDevsObject = await modelLoader('sq.glb');
+vedikDevsObject.position.setY(-3)
+vedikDevsObject.position.setZ(5)
+// vedikDevsObject.position.setX(-5)
 
-// const light = new Three.AmbientLight(0xffffff, 2)
-// // const helper = new Three.PointLightHelper(light, 1, 0x000000)
-// scene.add(light)
+scene.add(vedikDevsObject)
 
-// function render() {
-//   renderer.render(scene, camera)
-//   vedikDevsObject.rotateY(0.01)
-//   requestAnimationFrame(render)
-//   controls.update()
-// }
+const light = new Three.AmbientLight(0xffffff, 2)
+// const helper = new Three.PointLightHelper(light, 1, 0x000000)
+scene.add(light)
 
-// render()
+function render() {
+  renderer.render(scene, camera)
+  vedikDevsObject.rotateY(0.01)
+  requestAnimationFrame(render)
+  // controls.update()
+}
+
+render()
 
 
